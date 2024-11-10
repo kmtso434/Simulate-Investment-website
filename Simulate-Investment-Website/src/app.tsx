a86ef242a398f240e9e8e4ea466c678809597850
kmtso434 <122084476+kmtso434@users.noreply.github.com>
2024-11-10 23:42:49 +0900

    feat: prettier

--- Simulate-Investment-Website/src/app.tsx
+++ Simulate-Investment-Website/src/app.tsx
@@ -1,13 +1,13 @@
-import type { Component } from 'solid-js';
-import { css } from '../styled-system/css';
-import logo from './logo.svg';
-import { Button } from './components/ui/button';
+import type { Component } from "solid-js";
+import { css } from "../styled-system/css";
+import logo from "./logo.svg";
+import { Button } from "./components/ui/button";
 
 const App: Component = () => {
   return (
     <div>
-      <div class={css({ fontSize: "2xl", fontWeight: 'bold' })}>Hello 🐼!</div>
-      <Button>sdf</Button> 
+      <div class={css({ fontSize: "2xl", fontWeight: "bold" })}>Hello 🐼!</div>
+      <Button>sdf</Button>
     </div>
   );
 };
--- Simulate-Investment-Website/src/components/ui/combobox.tsx
+++ Simulate-Investment-Website/src/components/ui/combobox.tsx
@@ -1,2 +1,2 @@
-export { createListCollection } from '@ark-ui/solid'
-export * as Combobox from './styled/combobox'
+export { createListCollection } from "@ark-ui/solid";
+export * as Combobox from "./styled/combobox";
